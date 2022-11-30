d01b0b3af57a7b9716d66c2ab5a42f8b0f199b2b
‘zhongjiawen’ <1431674946@qq.com>
2022-12-01 01:21:10 +0800

    “主分支修改”

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //(exclude= DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class DemoApplication {
-
+     /*主分支修改1*/
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
